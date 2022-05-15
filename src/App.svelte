f7db7c1e1da124d0ab5f60f1702421654cb88127
henil0604 <henilmalaviya06@gmail.com>
2022-05-15 18:46:24 +0530

    feature: added links to semantic-ui and loaded example component

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,11 @@
 <script>
+
+	import HelloWorld from '@/components/HelloWorld';
+
 </script>
 
 <main>
-	<h1>Hello!</h1>
+	<HelloWorld />
 </main>
 
 <style>
@@ -13,13 +16,7 @@
 		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
+	
 	@media (min-width: 640px) {
 		main {
 			max-width: none;
